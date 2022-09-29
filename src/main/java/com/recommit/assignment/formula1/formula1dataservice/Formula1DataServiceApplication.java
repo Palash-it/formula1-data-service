98a12eb8c2a045ccded953b8c8358dc1aaf9636b
palash-recommit <palash.debnath@recommit.se>
2022-09-29 21:34:43 +0200

    Initial Commit: Spring boot project initialized with required dependencies

---
+++ src/main/java/com/recommit/assignment/formula1/formula1dataservice/Formula1DataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.recommit.assignment.formula1.formula1dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Formula1DataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Formula1DataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recommit/assignment/formula1/formula1dataservice/Formula1DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recommit.assignment.formula1.formula1dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Formula1DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
